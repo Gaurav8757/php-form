a04b3a985b887c46446a325c228aed5123b976e9
Gaurav Kumar <mr.gauravkumar8757@gmail.com>
2025-06-16 19:25:45 +0530

    updates

--- file.php
+++ file.php
@@ -1,3 +1,4 @@
+<!-- write file -->
 <?php
 $fileName = "temp.txt";
 $content = "Hello, this is a file named $fileName. It contains some PHP code that demonstrates basic functionality.";
@@ -12,4 +13,22 @@ if ($file) {
 }
 
 
+?>
+
+<!-- Read file then close -->
+<?php
+$fileName = "temp.txt";
+if (file_exists($fileName)) {
+    $file = fopen($fileName, "r");
+    if ($file) {
+        $content = fread($file, filesize($fileName));
+        fclose($file);
+        echo "Content of '$fileName':<br>";
+        echo nl2br(htmlspecialchars($content)); // Display content safely
+    } else {
+        echo "Failed to open file '$fileName'.<br>";
+    }
+} else {
+    echo "File '$fileName' does not exist.<br>";
+}
 ?>
